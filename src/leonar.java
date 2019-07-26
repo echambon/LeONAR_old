bf1db3448d546e238f495a0ecd2261d386f62df7
Emmanuel Chambon <contact@emmanuel-chambon.fr>
2019-07-26 10:11:01 +0200

    COM port enumeration debug test

--- src/leonar.java
+++ src/leonar.java
@@ -1,9 +1,27 @@
+import gnu.io.*;
+import java.util.Enumeration;
 
 public class leonar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Program Started!!!");
+		
+		CommPortIdentifier serialPortId;
+		
+		Enumeration enumComm;
+		
+		enumComm = CommPortIdentifier.getPortIdentifiers();
+		
+		while(enumComm.hasMoreElements())
+		{
+			serialPortId = (CommPortIdentifier)enumComm.nextElement();
+			if(serialPortId.getPortType() == CommPortIdentifier.PORT_SERIAL)
+			{
+				System.out.println(serialPortId.getName());
+			}
+		}
+		
+		System.out.println("Program Finished Sucessfully");
 	}
 
 }
