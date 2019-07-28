bc3b3514478d0ebee887ebb1e8ad8502c1174003
Emmanuel Chambon <contact@emmanuel-chambon.fr>
2019-07-28 12:07:24 +0200

    found working jSerialComm library

--- src/app/Leonar.java
+++ src/app/Leonar.java
@@ -1,68 +1,25 @@
 package app;
 
-/*import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
-public class Leonar extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-}*/
-
-//import gnu.io.*;
-//import gnss.*;
-import com.*;
-
-import java.io.IOException;
-//import java.util.Enumeration;
+import com.fazecast.jSerialComm.*;
 
 public class Leonar {
 
 	public static void main(String[] args) {
 		System.out.println("Program Started!!!");
-		
-		// debug Position
-		//Position test = new Position();
-		//Position test2 = new Position(2.0f, CardinalDirection.South, 4.5f, CardinalDirection.West);
-		
-		// debug com
-		
-		/*CommPortIdentifier serialPortId;
-		Enumeration enumComm;
-		
-		enumComm = CommPortIdentifier.getPortIdentifiers();
-		
-		while(enumComm.hasMoreElements()) {
-			serialPortId = (CommPortIdentifier)enumComm.nextElement();
-			if(serialPortId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
-				System.out.println(serialPortId.getName());
-			}
-		}*/
-		
-		// debug Communicator
+		SerialPort comPort = SerialPort.getCommPorts()[0];
+		comPort.openPort();
 		try {
-			Communicator test = new Communicator("COM3",9600);
-			test.closeUSB();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		   while (true)
+		   {
+		      while (comPort.bytesAvailable() == 0)
+		         Thread.sleep(20);
+
+		      byte[] readBuffer = new byte[comPort.bytesAvailable()];
+		      int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+		      System.out.println("Read " + numRead + " bytes.");
+		   }
+		} catch (Exception e) { e.printStackTrace(); }
+		comPort.closePort();
 		System.out.println("Program Finished Sucessfully");
 	}
 
