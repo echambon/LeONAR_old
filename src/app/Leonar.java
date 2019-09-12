09c083f8a92068dba772923cbaff434093ba907b
Emmanuel Chambon <contact@emmanuel-chambon.fr>
2019-09-12 22:14:21 +0200

    added existing COM port detection

--- src/app/Leonar.java
+++ src/app/Leonar.java
@@ -6,20 +6,26 @@ public class Leonar {
 
 	public static void main(String[] args) {
 		System.out.println("Program Started!!!");
-		SerialPort comPort = SerialPort.getCommPorts()[0];
-		comPort.openPort();
-		try {
-		   while (true)
-		   {
-		      while (comPort.bytesAvailable() == 0)
-		         Thread.sleep(20);
+		SerialPort[] comPorts = SerialPort.getCommPorts();
+		if(comPorts.length > 0) {
+			SerialPort comPort = comPorts[0];
+			comPort.openPort();
+			try {
+			   while (true)
+			   {
+			      while (comPort.bytesAvailable() == 0)
+			         Thread.sleep(20);
 
-		      byte[] readBuffer = new byte[comPort.bytesAvailable()];
-		      int numRead = comPort.readBytes(readBuffer, readBuffer.length);
-		      System.out.println("Read " + numRead + " bytes.");
-		   }
-		} catch (Exception e) { e.printStackTrace(); }
-		comPort.closePort();
+			      byte[] readBuffer = new byte[comPort.bytesAvailable()];
+			      int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+			      System.out.println("Read " + numRead + " bytes.");
+			   }
+			} catch (Exception e) { e.printStackTrace(); }
+			comPort.closePort();
+		} else {
+			System.out.println("No COM port found, connect receiver and restart");
+		}
+			
 		System.out.println("Program Finished Sucessfully");
 	}
 
